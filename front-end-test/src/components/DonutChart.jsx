b050c0905fd1313fec903138f0d03174de3e44f5
antonio-ramon <antonio-ramon-dev@outlook.com>
2023-05-05 01:03:01 -0300

    mainChart &DonutChart dinamic data

--- front-end-test/src/components/DonutChart.jsx
+++ front-end-test/src/components/DonutChart.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const DonutChart = () => {
+  // Define o estado inicial dos dados do gráfico para que ao carregar a página eles nao iniciem vazios
+  const [data, setData] = useState([
+    {
+      name: "Norte",
+      y: 20, // Define um valor aleatório entre 1 e 20 para cada fatia
+    },
+    {
+      name: "Nordeste",
+      y: 20,
+    },
+    {
+      name: "Sudeste",
+      y: 20,
+    },
+    {
+      name: "Centro Oeste",
+      y: 20,
+    },
+    {
+      name: "Sul",
+      y: 20,
+    },
+  ]);
+
+  // Cria um efeito para atualizar os dados do gráfico a cada 5 segundos
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Cria um novo array de objetos baseado nos dados atuais, com valores aleatórios para cada fatia do gráfico
+      const newData = data.map((item) => ({
+        ...item,
+        y: Math.floor(Math.random() * 20) + 1,
+      }));
+      // Define os novos dados no estado do componente
+      setData(newData);
+    }, 5000);
+
+    // Retorna uma função que limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(intervalId);
+  }, [data]); // Define a dependência como o estado atual do componente para evitar que o efeito seja recriado desnecessariamente
+
   const options = {
     chart: {
       type: "pie",
@@ -33,30 +73,9 @@ const DonutChart = () => {
     },
     series: [
       {
-        name: "Usuários",
+        name: "Usuários ativos",
         colorByPoint: true,
-        data: [
-          {
-            name: "Norte",
-            y: 20,
-          },
-          {
-            name: "Nordeste",
-            y: 20,
-          },
-          {
-            name: "Sudeste",
-            y: 20,
-          },
-          {
-            name: "Centro Oeste",
-            y: 20,
-          },
-          {
-            name: "Sul",
-            y: 20,
-          },
-        ],
+        data,
       },
     ],
   };
--- front-end-test/src/components/MainChart.jsx
+++ front-end-test/src/components/MainChart.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const MainChart = () => {
+  // Inicializa os estados de randomBrazil e randomMexico com valores pré-definidos para que ao carregar a página eles nao iniciem vazios
+  const [randomBrazil, setRandomBrazil] = useState([
+    2, 4.5, 5, 7.5, 6, 7, 5, 6, 8,
+  ]);
+  const [randomMexico, setRandomMexico] = useState([
+    0, 1, 3, 2.8, 4, 3, 3, 2.8, 5,
+  ]);
+
+  // Define o efeito que atualiza os estados a cada 5 segundos
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Cria um array com valores aleatórios para randomBrazil
+      const randomBrazilData = [];
+      for (let i = 0; i < 9; i++) {
+        randomBrazilData.push(Math.floor(Math.random() * 1000));
+      }
+      // Atualiza o estado de randomBrazil com os novos valores aleatórios
+      setRandomBrazil(randomBrazilData);
+
+      // Cria um array com valores aleatórios para randomMexico
+      const randomMexicoData = [];
+      for (let i = 0; i < 9; i++) {
+        randomMexicoData.push(Math.floor(Math.random() * 1000));
+      }
+      // Atualiza o estado de randomMexico com os novos valores aleatórios
+
+      setRandomMexico(randomMexicoData);
+    }, 5000);
+
+    // Retorna uma função que limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(interval);
+  }, []);
+
   const options = {
     chart: {
       height: 350,
@@ -20,7 +53,7 @@ const MainChart = () => {
       y: 25,
     },
     xAxis: {
-      startOnTick: true,
+      startOnTick: false,
       categories: ["", "", "", "", "", "", "", "", ""],
       gridLineWidth: 1.2,
       gridLineDashStyle: "Dash",
@@ -56,7 +89,7 @@ const MainChart = () => {
     series: [
       {
         name: "México",
-        data: [0, 1, 3, 2.8, 4, 3, 3, 2.8, 5],
+        data: randomMexico,
         color: "#8C54FF",
         lineWidth: 2,
         marker: {
@@ -68,7 +101,7 @@ const MainChart = () => {
       },
       {
         name: "Brasil",
-        data: [2, 4.5, 5, 7.5, 6, 7, 5, 6, 8],
+        data: randomBrazil,
         color: "#2cd9c5",
         lineWidth: 2,
 
@@ -98,7 +131,11 @@ const MainChart = () => {
     },
   };
 
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 };
 
 export default MainChart;
